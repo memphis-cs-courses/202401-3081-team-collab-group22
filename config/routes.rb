d2d4a8c8ce217ada424285484843023559d5e19f
Adrien Jones <tjones69@memphis.edu>
2024-04-22 13:46:53 -0500

    Changed username to lowercase

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'TJMarie', to: 'pages#TJMarie', as: 'TJMarie'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -9,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+  get 'TJMarie', to: 'pages#TJMarie', as: 'tjmarie'
 end
